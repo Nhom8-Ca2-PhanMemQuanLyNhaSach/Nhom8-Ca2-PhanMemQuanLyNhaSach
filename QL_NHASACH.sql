f42a929bed912c0128b9b0f37c649970367fee5f
hoangmy121097 <42493828+hoangmy121097@users.noreply.github.com>
2018-09-24 14:28:31 +0700

    Add files via upload

--- QL_NHASACH.sql
+++ QL_NHASACH.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      Microsoft SQL Server 2012                    */
-/* Created on:     9/17/2018 3:22:29 PM                         */
+/* Created on:     9/24/2018 2:03:48 PM                         */
 /*==============================================================*/
 
 
@@ -41,6 +41,13 @@ go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('HANGHOA') and o.name = 'FK_HANGHOA_RELATIONS_KHUYENMA')
+alter table HANGHOA
+   drop constraint FK_HANGHOA_RELATIONS_KHUYENMA
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
    where r.fkeyid = object_id('HOADON') and o.name = 'FK_HOADON_RELATIONS_NHANVIEN')
 alter table HOADON
    drop constraint FK_HOADON_RELATIONS_NHANVIEN
@@ -58,6 +65,34 @@ if exists (select 1
    where r.fkeyid = object_id('KHACHHANG') and o.name = 'FK_KHACHHAN_RELATIONS_LOAIKHAC')
 alter table KHACHHANG
    drop constraint FK_KHACHHAN_RELATIONS_LOAIKHAC
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('NHOMNGUOIDUNG_NV') and o.name = 'FK_NHOMNGUO_NHOMNGUOI_NHANVIEN')
+alter table NHOMNGUOIDUNG_NV
+   drop constraint FK_NHOMNGUO_NHOMNGUOI_NHANVIEN
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('NHOMNGUOIDUNG_NV') and o.name = 'FK_NHOMNGUO_NHOMNGUOI_NHOMNGUO')
+alter table NHOMNGUOIDUNG_NV
+   drop constraint FK_NHOMNGUO_NHOMNGUOI_NHOMNGUO
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('PHANQUYEN') and o.name = 'FK_PHANQUYE_PHANQUYEN_NHOMNGUO')
+alter table PHANQUYEN
+   drop constraint FK_PHANQUYE_PHANQUYEN_NHOMNGUO
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('PHANQUYEN') and o.name = 'FK_PHANQUYE_PHANQUYEN_DM_MANHI')
+alter table PHANQUYEN
+   drop constraint FK_PHANQUYE_PHANQUYEN_DM_MANHI
 go
 
 if exists (select 1
@@ -115,6 +150,22 @@ if exists (select 1
            where  id = object_id('CT_PHIEUNHAP')
             and   type = 'U')
    drop table CT_PHIEUNHAP
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('DM_MANHINH')
+            and   type = 'U')
+   drop table DM_MANHINH
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('HANGHOA')
+            and   name  = 'RELATIONSHIP_7_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index HANGHOA.RELATIONSHIP_7_FK
 go
 
 if exists (select 1
@@ -176,6 +227,13 @@ go
 
 if exists (select 1
             from  sysobjects
+           where  id = object_id('KHUYENMAI')
+            and   type = 'U')
+   drop table KHUYENMAI
+go
+
+if exists (select 1
+            from  sysobjects
            where  id = object_id('LOAIHANGHOA')
             and   type = 'U')
    drop table LOAIHANGHOA
@@ -203,6 +261,63 @@ if exists (select 1
 go
 
 if exists (select 1
+            from  sysobjects
+           where  id = object_id('NHOMNGUOIDUNG')
+            and   type = 'U')
+   drop table NHOMNGUOIDUNG
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('NHOMNGUOIDUNG_NV')
+            and   name  = 'NHOMNGUOIDUNG_NV2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index NHOMNGUOIDUNG_NV.NHOMNGUOIDUNG_NV2_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('NHOMNGUOIDUNG_NV')
+            and   name  = 'NHOMNGUOIDUNG_NV_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index NHOMNGUOIDUNG_NV.NHOMNGUOIDUNG_NV_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('NHOMNGUOIDUNG_NV')
+            and   type = 'U')
+   drop table NHOMNGUOIDUNG_NV
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PHANQUYEN')
+            and   name  = 'PHANQUYEN2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index PHANQUYEN.PHANQUYEN2_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PHANQUYEN')
+            and   name  = 'PHANQUYEN_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index PHANQUYEN.PHANQUYEN_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('PHANQUYEN')
+            and   type = 'U')
+   drop table PHANQUYEN
+go
+
+if exists (select 1
             from  sysindexes
            where  id    = object_id('PHIEUNHAP')
             and   name  = 'RELATIONSHIP_5_FK'
@@ -222,8 +337,8 @@ go
 /* Table: CHITIET_HOADON                                        */
 /*==============================================================*/
 create table CHITIET_HOADON (
-   MAHD                 nvarchar(5)          not null,
-   MAHH                 nvarchar(5)          not null,
+   MAHD                 nchar(5)             not null,
+   MAHH                 nchar(5)             not null,
    SOLUONG              int                  null,
    DONGIA               float                null,
    constraint PK_CHITIET_HOADON primary key nonclustered (MAHD, MAHH)
@@ -254,8 +369,8 @@ go
 /* Table: CT_PHIEUNHAP                                          */
 /*==============================================================*/
 create table CT_PHIEUNHAP (
-   MAPN                 nvarchar(5)          not null,
-   MAHH                 nvarchar(5)          not null,
+   MAPN                 nchar(5)             not null,
+   MAHH                 nchar(5)             not null,
    constraint PK_CT_PHIEUNHAP primary key nonclustered (MAPN, MAHH)
 )
 go
@@ -281,14 +396,26 @@ create nonclustered index CT_PHIEUNHAP2_FK on CT_PHIEUNHAP (MAHH ASC)
 go
 
 /*==============================================================*/
+/* Table: DM_MANHINH                                            */
+/*==============================================================*/
+create table DM_MANHINH (
+   MAMANHINH            nchar(5)             not null,
+   TENMANHINH           nvarchar(50)         null,
+   constraint PK_DM_MANHINH primary key (MAMANHINH)
+)
+go
+
+/*==============================================================*/
 /* Table: HANGHOA                                               */
 /*==============================================================*/
 create table HANGHOA (
-   MAHH                 nvarchar(5)          not null,
-   MALOAI_HH            nvarchar(5)          null,
+   MAHH                 nchar(5)             not null,
+   MALOAI_HH            nchar(5)             null,
+   MAKHUYENMAI          nchar(5)             null,
    TENHH                nvarchar(30)         null,
    DVT_HANGHOA          nvarchar(10)         null,
    TINHTRANG            nvarchar(10)         null,
+   HINHANH_HANGHOA      image                null,
    constraint PK_HANGHOA primary key (MAHH)
 )
 go
@@ -304,12 +431,22 @@ create nonclustered index RELATIONSHIP_6_FK on HANGHOA (MALOAI_HH ASC)
 go
 
 /*==============================================================*/
+/* Index: RELATIONSHIP_7_FK                                     */
+/*==============================================================*/
+
+
+
+
+create nonclustered index RELATIONSHIP_7_FK on HANGHOA (MAKHUYENMAI ASC)
+go
+
+/*==============================================================*/
 /* Table: HOADON                                                */
 /*==============================================================*/
 create table HOADON (
-   MAHD                 nvarchar(5)          not null,
-   MANV                 nvarchar(5)          null,
-   MAKH                 nvarchar(5)          null,
+   MAHD                 nchar(5)             not null,
+   MANV                 nchar(5)             null,
+   MAKH                 nchar(5)             null,
    NGAYLAP              datetime             null,
    constraint PK_HOADON primary key (MAHD)
 )
@@ -339,11 +476,12 @@ go
 /* Table: KHACHHANG                                             */
 /*==============================================================*/
 create table KHACHHANG (
-   MAKH                 nvarchar(5)          not null,
-   MALOAI_KH            nvarchar(5)          null,
+   MAKH                 nchar(5)             not null,
+   MALOAI_KH            nchar(5)             null,
    TENKH                nvarchar(30)         null,
    DIACHI_KH            nvarchar(50)         null,
    SDT_KH               nvarchar(11)         null,
+   CHIETKHAU            float                null,
    constraint PK_KHACHHANG primary key (MAKH)
 )
 go
@@ -359,10 +497,21 @@ create nonclustered index RELATIONSHIP_4_FK on KHACHHANG (MALOAI_KH ASC)
 go
 
 /*==============================================================*/
+/* Table: KHUYENMAI                                             */
+/*==============================================================*/
+create table KHUYENMAI (
+   MAKHUYENMAI          nchar(5)             not null,
+   TENKHUYENMAI         nvarchar(30)         null,
+   TILEGIAMGIA          float                null,
+   constraint PK_KHUYENMAI primary key (MAKHUYENMAI)
+)
+go
+
+/*==============================================================*/
 /* Table: LOAIHANGHOA                                           */
 /*==============================================================*/
 create table LOAIHANGHOA (
-   MALOAI_HH            nvarchar(5)          not null,
+   MALOAI_HH            nchar(5)             not null,
    TENLOAI_HH           nvarchar(30)         null,
    constraint PK_LOAIHANGHOA primary key (MALOAI_HH)
 )
@@ -372,7 +521,7 @@ go
 /* Table: LOAIKHACHHANG                                         */
 /*==============================================================*/
 create table LOAIKHACHHANG (
-   MALOAI_KH            nvarchar(5)          not null,
+   MALOAI_KH            nchar(5)             not null,
    TENLOAI_KH           nvarchar(30)         null,
    constraint PK_LOAIKHACHHANG primary key (MALOAI_KH)
 )
@@ -382,7 +531,7 @@ go
 /* Table: NHACUNGCAP                                            */
 /*==============================================================*/
 create table NHACUNGCAP (
-   MANCC                nvarchar(5)          not null,
+   MANCC                nchar(5)             not null,
    TENNCC               nvarchar(30)         null,
    DIACHI_NCC           nvarchar(50)         null,
    SDT_NCC              nvarchar(11)         null,
@@ -394,7 +543,8 @@ go
 /* Table: NHANVIEN                                              */
 /*==============================================================*/
 create table NHANVIEN (
-   MANV                 nvarchar(5)          not null,
+   MANV                 nchar(5)             not null,
+   MATKHAU              nvarchar(10)         null,
    TENNV                nvarchar(30)         null,
    DIACHI_NV            nvarchar(50)         null,
    SDT_NV               nvarchar(11)         null,
@@ -403,11 +553,83 @@ create table NHANVIEN (
 go
 
 /*==============================================================*/
+/* Table: NHOMNGUOIDUNG                                         */
+/*==============================================================*/
+create table NHOMNGUOIDUNG (
+   MANHOM               nchar(5)             not null,
+   TENNHOM              nvarchar(50)         null,
+   constraint PK_NHOMNGUOIDUNG primary key (MANHOM)
+)
+go
+
+/*==============================================================*/
+/* Table: NHOMNGUOIDUNG_NV                                      */
+/*==============================================================*/
+create table NHOMNGUOIDUNG_NV (
+   MANV                 nchar(5)             not null,
+   MANHOM               nchar(5)             not null,
+   GHICHU               nvarchar(50)         null,
+   constraint PK_NHOMNGUOIDUNG_NV primary key nonclustered (MANV, MANHOM)
+)
+go
+
+/*==============================================================*/
+/* Index: NHOMNGUOIDUNG_NV_FK                                   */
+/*==============================================================*/
+
+
+
+
+create nonclustered index NHOMNGUOIDUNG_NV_FK on NHOMNGUOIDUNG_NV (MANV ASC)
+go
+
+/*==============================================================*/
+/* Index: NHOMNGUOIDUNG_NV2_FK                                  */
+/*==============================================================*/
+
+
+
+
+create nonclustered index NHOMNGUOIDUNG_NV2_FK on NHOMNGUOIDUNG_NV (MANHOM ASC)
+go
+
+/*==============================================================*/
+/* Table: PHANQUYEN                                             */
+/*==============================================================*/
+create table PHANQUYEN (
+   MANHOM               nchar(5)             not null,
+   MAMANHINH            nchar(5)             not null,
+   COQUYEN              nvarchar(50)         null,
+   constraint PK_PHANQUYEN primary key nonclustered (MANHOM, MAMANHINH)
+)
+go
+
+/*==============================================================*/
+/* Index: PHANQUYEN_FK                                          */
+/*==============================================================*/
+
+
+
+
+create nonclustered index PHANQUYEN_FK on PHANQUYEN (MANHOM ASC)
+go
+
+/*==============================================================*/
+/* Index: PHANQUYEN2_FK                                         */
+/*==============================================================*/
+
+
+
+
+create nonclustered index PHANQUYEN2_FK on PHANQUYEN (MAMANHINH ASC)
+go
+
+/*==============================================================*/
 /* Table: PHIEUNHAP                                             */
 /*==============================================================*/
 create table PHIEUNHAP (
-   MAPN                 nvarchar(5)          not null,
-   MANCC                nvarchar(5)          null,
+   MAPN                 nchar(5)             not null,
+   MANCC                nchar(5)             null,
    NGAYNHAP             datetime             null,
    constraint PK_PHIEUNHAP primary key (MAPN)
 )
@@ -448,6 +670,11 @@ alter table HANGHOA
       references LOAIHANGHOA (MALOAI_HH)
 go
 
+alter table HANGHOA
+   add constraint FK_HANGHOA_RELATIONS_KHUYENMA foreign key (MAKHUYENMAI)
+      references KHUYENMAI (MAKHUYENMAI)
+go
+
 alter table HOADON
    add constraint FK_HOADON_RELATIONS_NHANVIEN foreign key (MANV)
       references NHANVIEN (MANV)
@@ -461,6 +688,26 @@ go
 alter table KHACHHANG
    add constraint FK_KHACHHAN_RELATIONS_LOAIKHAC foreign key (MALOAI_KH)
       references LOAIKHACHHANG (MALOAI_KH)
+go
+
+alter table NHOMNGUOIDUNG_NV
+   add constraint FK_NHOMNGUO_NHOMNGUOI_NHANVIEN foreign key (MANV)
+      references NHANVIEN (MANV)
+go
+
+alter table NHOMNGUOIDUNG_NV
+   add constraint FK_NHOMNGUO_NHOMNGUOI_NHOMNGUO foreign key (MANHOM)
+      references NHOMNGUOIDUNG (MANHOM)
+go
+
+alter table PHANQUYEN
+   add constraint FK_PHANQUYE_PHANQUYEN_NHOMNGUO foreign key (MANHOM)
+      references NHOMNGUOIDUNG (MANHOM)
+go
+
+alter table PHANQUYEN
+   add constraint FK_PHANQUYE_PHANQUYEN_DM_MANHI foreign key (MAMANHINH)
+      references DM_MANHINH (MAMANHINH)
 go
 
 alter table PHIEUNHAP
